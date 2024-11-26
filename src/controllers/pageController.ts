dd0f77a70a0df59ce87c371abfb76b86e791ae07
Paulo Feltz Pinheiro <pauloupgrad@gmail.com>
2024-11-26 13:21:29 -0300

    Criando função do active menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {    
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg',
@@ -11,6 +13,7 @@ export const home = (req: Request, res: Response) => {
 }
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -19,6 +22,7 @@ export const dogs = (req: Request, res: Response) => {
 }
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -27,6 +31,7 @@ export const cats = (req: Request, res: Response) => {
 }
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner: {
             title: 'Peíxes',
             background: 'banner_fish.jpg'
