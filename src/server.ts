d9dfc23c536b0bc00da28cf1278b6e25c31abb34
Paulo Feltz Pinheiro <pauloupgrad@gmail.com>
2024-11-26 15:05:49 -0300

    Final do projeto 1.0

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req,res) => {
-    res.send('Página não encontrado');
+    res.render('pages/404');
 });
 
 // SERVIDOR RODANDO
